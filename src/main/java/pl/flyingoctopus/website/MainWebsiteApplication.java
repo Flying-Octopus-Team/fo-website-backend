4f8a06a2312336ddf1d56fdd009c9038a7e26968
Piotr 'dyedMagenta' Frankiewicz <dyedmagenta@gmail.com>
2021-04-26 21:50:52 +0200

    Add initial project

---
+++ src/main/java/pl/flyingoctopus/website/MainWebsiteApplication.java
@@ -0,0 +1,13 @@
+package pl.flyingoctopus.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/flyingoctopus/website/MainWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.flyingoctopus.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
