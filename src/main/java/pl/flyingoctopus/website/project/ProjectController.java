aa894f9f83c2e50cd1de1c045fcf3e6dd3d1285e
Piotr 'dyedMagenta' Frankiewicz <dyedmagenta@gmail.com>
2021-05-15 18:30:23 +0200

    #5 Add keycloak and basic configuration

--- src/main/java/pl/flyingoctopus/website/project/ProjectController.java
+++ src/main/java/pl/flyingoctopus/website/project/ProjectController.java
@@ -2,9 +2,11 @@ package pl.flyingoctopus.website.project;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("projects")
 public class ProjectController {
 
 
