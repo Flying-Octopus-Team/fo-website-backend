0dc80853fd4ab31310cdd1869dfd482886b3c210
Piotr Frankiewicz <frankiewicz@o2.pl>
2021-06-10 18:03:47 +0200

    #5 Disble cors and csrf (#15)

--- src/main/java/pl/flyingoctopus/website/project/ProjectController.java
+++ src/main/java/pl/flyingoctopus/website/project/ProjectController.java
@@ -2,6 +2,7 @@ package pl.flyingoctopus.website.project;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("projects")
 public class ProjectController {
 
-
     @GetMapping
     public String hello() {
         return "hello";
     }
 
     @PostMapping
-    public String hello(String name) {
-        return "hello, " + name;
+    public String hello(@RequestBody HelloDTO hello) {
+        return "hello, " + hello.name;
     }
 
 }
